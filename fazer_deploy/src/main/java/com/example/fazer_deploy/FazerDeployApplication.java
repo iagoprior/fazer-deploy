33fa344e997b4a856af84355925d414744888a47
Iago Prior <iagoi.a.s.p_@hotmail.com>
2020-04-12 15:02:43 -0300

    fazer-deploy

---
+++ fazer_deploy/src/main/java/com/example/fazer_deploy/FazerDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.fazer_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazerDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazerDeployApplication.class, args);
+	}
+
+}
---
+++ fazer_deploy/src/test/java/com/example/fazer_deploy/FazerDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fazer_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazerDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
